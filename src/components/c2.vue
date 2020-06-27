5d56cd3ff5596b849a38a38c5f5abaf45fa50593
feirâYR <526230112@qq.com>
2020-06-27 17:41:50 +0800

    ä主主分支提交组件c2

---
+++ src/components/c2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p>第二页</p>
+<!--      <span>{{$route.query.name}}</span>-->
+      <span>{{$route.params.name}},{{$route.params.id}}</span><br>
+      <span>S:{{$store.state.name}}</span><br>
+        <span>g:{{$store.getters.new_name}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "c2"
+    }
+</script>
+
+<style scoped>
+
+</style>
